39d6b6ab63bf88a072479f94ad58be680191ec61
tynakpinska <m.k.kepinska@gmail.com>
2020-04-26 21:23:24 +0200

    Updates

---
+++ precache-manifest.3bc5822a02b7b66e135f3ebeea8bf3f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128ce63ddf9e37ec90cc3bd328bbc1cc",
+    "url": "/music-quiz/index.html"
+  },
+  {
+    "revision": "9bb4cfbf9f6008a0ec04",
+    "url": "/music-quiz/static/css/main.6c4ebef7.chunk.css"
+  },
+  {
+    "revision": "183c34d42b2f799a79d0",
+    "url": "/music-quiz/static/js/2.68fe4822.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/music-quiz/static/js/2.68fe4822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb4cfbf9f6008a0ec04",
+    "url": "/music-quiz/static/js/main.bdcdda2c.chunk.js"
+  },
+  {
+    "revision": "0948149610a88bb477d5",
+    "url": "/music-quiz/static/js/runtime-main.07baa274.js"
+  },
+  {
+    "revision": "ee80dc3f504f26412295fcdf110f59d2",
+    "url": "/music-quiz/static/media/music.ee80dc3f.jpg"
+  }
+]);
